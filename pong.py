4bdf686157a6adb5e0f3c5cee5d6998f69a2c3b7
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-09-12 20:15:30 -0400

    created an AI

--- pong.py
+++ pong.py
@@ -130,3 +130,11 @@ while True:
         ball.setx(-340)
         ball.dx *= -1
         winsound.PlaySound('bounce.wav', winsound.SND_ASYNC)
+
+    # AI player
+
+    if paddle_b.ycor() < ball.ycor() and abs(paddle_b.ycor() - ball.ycor()) > 10:
+        paddle_b_up()
+    
+    elif paddle_b.ycor() > ball.ycor() and abs(paddle_b.ycor() - ball.ycor()) > 10:
+        paddle_b_down()
